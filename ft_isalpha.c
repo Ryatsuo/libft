4117b5b92d372553587128dc54076bee8891ea57
Dobele Enzo <enzo.dobele@yahoo.fr>
2025-03-16 01:59:08 +0100

    ft_isalpha fini

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,1 +1,22 @@
-int     
+int ft_isalpha(unsigned char *tab)
+{
+    int i;
+
+    i = 0;
+    while(tab[i])
+    {
+        if ((tab[i] < 'a' || tab[i] > 'z') && 
+            (tab[i] < 'A' || tab[i] > 'Z'))
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+/*#include <stdio.h>
+int main ()
+{
+    unsigned char tab[] = "coucou";
+    printf("resultat %d\n", ft_isalpha(tab));
+    return 0;
+}*/

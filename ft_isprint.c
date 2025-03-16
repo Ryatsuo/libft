482bc1cd7c19c5c9b593a386eb31598baef6ff23
Dobele Enzo <enzo.dobele@yahoo.fr>
2025-03-16 18:12:42 +0100

    isprit fini

---
+++ ft_isprint.c
@@ -0,0 +1,15 @@
+int	ft_isprint(int c)
+{
+	if (c >= 32 && c <= 126)
+		return (1);
+	return (0);
+}
+/*
+#include <stdio.h>
+int main (int ac, char **av)
+{
+	if (ac != 2)
+		return (1);
+	printf("la valueur de retour est %d\n", ft_isprint(av[1][0]));
+	return (0);
+}*/

aa4503e90ed220f15a1c52c996a5f0426b0a977b
Dobele Enzo <enzo.dobele@yahoo.fr>
2025-03-16 19:59:26 +0100

    ft_memset fini

---
+++ ft_memset.c
@@ -0,0 +1,24 @@
+void *ft_memset(void *s, int c, int n)
+{
+	int i;
+	unsigned char *tab;
+
+	tab = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		tab[i] = (unsigned char)c;
+		i++;
+	}
+	return (s);
+}
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int main (int ac, char **av)
+{
+	if (ac != 4)
+		return (1);
+	printf("le tableau de char modifier est devenu : %s\n", (char *)ft_memset(av[1], (av[2][0]), atoi(av[3])));
+	return (0);
+}*/

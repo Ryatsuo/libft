ee075dc1e368e63ca7897a6e4ad4711fe82e1e1b
Dobele Enzo <enzo.dobele@yahoo.fr>
2025-03-16 16:19:36 +0100

    isalnum fini manque header

---
+++ ft_isalnum.c
@@ -0,0 +1,17 @@
+int	ft_isalnum(int c)
+{
+	if ((c >= 'a' && c <= 'z')
+		|| (c >= 'A' && c <= 'Z')
+		|| (c >= '0' && c <= '9'))
+		return (1);
+	return (0);
+}
+/*
+#include <stdio.h>
+int main (int ac, char **av)
+{
+	if (ac != 2)
+		return (1);
+	printf("la valeur de retour est %d\n", ft_isalnum(av[1][0]));
+	return (0);
+}*/

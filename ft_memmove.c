4fcba3e50092a98ad3c18d857eed3b9d5b163553
Dobele Enzo <enzo.dobele@yahoo.fr>
2025-03-16 22:23:02 +0100

    memmove fini

---
+++ ft_memmove.c
@@ -0,0 +1,39 @@
+void	*ft_memmove(void *dest, const void *src, int n)
+{
+	int					i;
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (d > s)
+	{
+		i = n;
+		while (i > 0)
+		{
+			i--;
+			d[i] = s[i];
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (dest);
+}
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main (int ac, char **av)
+{
+	if (ac != 3)
+		return (1);
+	printf("%s\n", (unsigned char *)ft_memmove(av[1], &av[1][2], atoi(av[2])));
+	return (0);
+}*/

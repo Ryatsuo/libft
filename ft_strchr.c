218afb371c656a737454c95178e1b9f8bd95de4a
Dobele Enzo <enzo.dobele@yahoo.fr>
2025-03-17 03:08:58 +0100

    strchr fini

---
+++ ft_strchr.c
@@ -0,0 +1,24 @@
+char	*strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)&s[i]);
+	return (0);
+}
+/*
+#include <stdio.h>
+int main (int ac, char **av)
+{
+	if(ac == 1)
+		return (1);
+	printf("l'occurence a ete trouve ici : %s", strchr(av[1], av[2][0]));
+	return (0);
+}*/

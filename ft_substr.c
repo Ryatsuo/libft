26901f3647a4bf26b43893636067b3c6761f5b49
Dobele Enzo <enzo.dobele@yahoo.fr>
2025-03-18 06:07:08 +0100

    substr fini

---
+++ ft_substr.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+
+char	*ft_strscpy(char const *c, char *tab, unsigned int start, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (c[start] && i < len)
+	{
+		tab[i] = c[start];
+		start++;
+		i++;
+	}
+	tab[i] = '\0';
+	return (tab);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(char const *c, unsigned int start, size_t len)
+{
+	size_t			i;
+	char			*tab;
+	size_t			c_len;
+	unsigned int	tmp_start;
+
+	i = 0;
+	c_len = ft_strlen(c);
+	tmp_start = start;
+	if (len == 0 || start >= c_len)
+	{
+		tab = malloc(1);
+		if (!tab)
+			return (NULL);
+		tab[0] = '\0';
+		return (tab);
+	}
+	while (c[start] && i < len)
+	{
+		i++;
+		start++;
+	}
+	tab = malloc(sizeof(char) * (i + 1));
+	if (!tab)
+		return (NULL);
+	return (ft_strscpy(c, tab, tmp_start, i));
+}
+/*
+#include <stdio.h>
+int main (void)
+{
+	char const a[] = "coucou coucou les amis";
+	char *tab = ft_substr(a, 7, 14);
+	printf("le tableau a pour valeur :%s\n", tab);
+	free(tab);
+	return (0);
+}*/

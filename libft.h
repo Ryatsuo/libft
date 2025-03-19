fcd8a56f3ecc9d34d8cff100fb16faccad10802b
Dobele Enzo <enzo.dobele@yahoo.fr>
2025-03-19 04:59:05 +0100

    makefile et .h à jour (ft_strtrim)

--- libft.h
+++ libft.h
@@ -27,4 +27,7 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_substr(char const *c, unsigned int start, size_t len);
 int	ft_tolower(int c);
 int	ft_toupper(int c);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *c, unsigned int start, size_t len);
+char	*ft_strjoin(const char *str1, const char *str2);
 #endif

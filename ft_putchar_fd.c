482baa10dc3bd919a8dde2d0a7e6ea3fa770a0e7
Dobele Enzo <enzo.dobele@yahoo.fr>
2025-03-22 03:56:24 +0100

    ft_putchar_fd fini et ajoute au makefile et .h

---
+++ ft_putchar_fd.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+/*
+int main(void)
+{
+	int i = 0;
+	char a[] = "coucou les amis";
+	while (a[i])
+	{
+		ft_putchar_fd(a[i], 1);
+		i++;
+	}
+	return (0);
+}*/
